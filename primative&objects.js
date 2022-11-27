bab36bbffb34ac2b5b2f90d30ba588f3fc6ff22b
faheem <darkrabelstudy@gmail.com>
2022-11-27 20:03:05 +0600

    premative and object done

---
+++ primative&objects.js
@@ -0,0 +1,18 @@
+// premative and unpremative 
+// There are 7 premative data types  
+let a=null
+let b = 345
+let c = true //! also can be false
+let d = BigInt("453")
+let e = "harry "
+let f = Symbol("I am a nice symboal ")
+let g = undefined
+//* objects in js 
+const item = {
+    "male":true,
+    "goodstudent":true,
+    "cancode":true,
+    "age":67
+    
+}
+console.log(item)
