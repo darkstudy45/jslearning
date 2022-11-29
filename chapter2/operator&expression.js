bdae20018c405d5d2e5390f1e528dd84a8f3381d
faheem <darkrabelstudy@gmail.com>
2022-11-29 20:36:41 +0600

    re update

---
+++ chapter2/operator&expression.js
@@ -0,0 +1,29 @@
+console.log("operators in js ");
+// power operator
+let a=41;
+let b=5;
+let a2=41
+let b2=5
+console.log("a**b =" , a**b);
+
+//! modular operator 
+console.log("a%b =", a%b);//#ভাগশেষ a/b করার পর যে ভাগশেষ থাকবে তাই pring hobe.
+
+//! increment operator 
+console.log("a++",a++) //# এখানে a  আগে প্রিন্ট হবে increment পরে হবে । 
+
+console.log("after increment a=",a)//# এখানে a=42 হবে
+
+
+console.log("++a =",++a2) //# এখানে a আগে increment হবে পরে print হবে। 
+ 
+
+//! decrement operator 
+
+console.log("b--",b--) //# এখানে a  আগে প্রিন্ট হবে increment পরে হবে । 
+
+console.log("after decrement b=",b)//# এখানে a=4 হবে
+
+
+
+console.log("--b =",--b2) //# এখানে b আগে decrement হবে পরে print হবে। 
