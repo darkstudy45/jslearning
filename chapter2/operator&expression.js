b74be785af67623ec0dc9c23f0bb2b58daa28776
faheem <darkrabelstudy@gmail.com>
2022-11-29 21:56:21 +0600

    all done with operator and expression

--- chapter2/operator&expression.js
+++ chapter2/operator&expression.js
@@ -1,29 +1,76 @@
 console.log("operators in js ");
 // power operator
-let a=41;
-let b=5;
-let a2=41
-let b2=5
-console.log("a**b =" , a**b);
+let a = 41;
+let b = 5;
+let a2 = 41
+let b2 = 5
+console.log("a**b =", a ** b);
 
 //! modular operator 
-console.log("a%b =", a%b);//#ভাগশেষ a/b করার পর যে ভাগশেষ থাকবে তাই pring hobe.
+console.log("a%b =", a % b);//#ভাগশেষ a/b করার পর যে ভাগশেষ থাকবে তাই pring hobe.
 
 //! increment operator 
-console.log("a++",a++) //# এখানে a  আগে প্রিন্ট হবে increment পরে হবে । 
+console.log("a++ =", a++) //# এখানে a  আগে প্রিন্ট হবে increment পরে হবে । 
 
-console.log("after increment a=",a)//# এখানে a=42 হবে
+console.log("after increment a=", a)//# এখানে a=42 হবে
 
 
-console.log("++a =",++a2) //# এখানে a আগে increment হবে পরে print হবে। 
- 
+console.log("++a =", ++a2) //# এখানে a আগে increment হবে পরে print হবে। 
+
 
 //! decrement operator 
 
-console.log("b--",b--) //# এখানে a  আগে প্রিন্ট হবে increment পরে হবে । 
+console.log("b--", b--) //# এখানে a  আগে প্রিন্ট হবে increment পরে হবে । 
 
-console.log("after decrement b=",b)//# এখানে a=4 হবে
+console.log("after decrement b=", b)//# এখানে a=4 হবে
 
 
 
-console.log("--b =",--b2) //# এখানে b আগে decrement হবে পরে print হবে। 
+console.log("--b =", --b2) //# এখানে b আগে decrement হবে পরে print হবে। 
+
+
+
+//! assignment operator 
+//# =    ------>>>>>      x=y
+//#  +=   ------>>>>>     x=x+y 
+//# -=   ------>>>>>      x=x-y
+//# /=   ------>>>>>      x=x/y
+//# %/   ------>>>>>      x=x**y
+
+
+
+let x1 =30;
+let x2 =30;
+let x3 =30;
+let x4 =30;
+let x5 =30;
+let x6 =30;
+let x7 =30;
+
+console.log(x1 =5);
+console.log(x2+=5); 
+console.log(x3-=5);
+
+console.log(x5/=5);
+console.log(x6 %=5); //# vagsesh 
+
+
+
+//! composition operator
+//  == equal to 
+//#     != not equal 
+//#    ====equal and same type 
+//#    !== not equal and not same type 
+//#    > greater than 
+//#    < less than 
+//#    >= greater than or equal to 
+//#    <= lesser than and equal to 
+//#    ? 
+
+
+//! logical operator  
+//# it operates on bulian value
+
+// #         && ------->     logical and 
+// #         || ------->     logical or 
+// #         !  ------->     logical net make true --> false and make false --> true
