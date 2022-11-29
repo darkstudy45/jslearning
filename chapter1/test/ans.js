7f594837b115aaff0819e091dc8ad5da9688ae55
faheem <darkrabelstudy@gmail.com>
2022-11-29 20:03:03 +0600

    all done

---
+++ chapter1/test/ans.js
@@ -0,0 +1,30 @@
+//*Q1
+let a = "faheem";
+
+let b= 2;
+// let c = a+b;
+console.log(a+b)
+//*Q2
+
+
+console.log(typeof (a+b));
+
+//*Q3
+const a1 ={
+    name: "faheem",
+    age: 18,
+    isgood : "yes"
+}
+a1["friend"]="someone " //! this should add friend in the a1 object . \
+console.log(a1);
+//*Q4
+
+
+const dict = {
+    phoenix:"a person or thing that has become renewed or restored after suffering calamity or apparent annihilation.",
+    gingerly:"with great care or caution",
+    plastique:"a ballet technique for mastering the art of slow, controlled movement and statuelike posing."
+
+}
+console.log(dict.phoenix)
+
