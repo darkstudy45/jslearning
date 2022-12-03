1f5d4b44c18b9756468921ad580dd6969f0d0670
faheem <darkrabelstudy@gmail.com>
2022-12-03 14:42:03 +0600

    all done with while loop

---
+++ chapter3/whileloops.js
@@ -0,0 +1,19 @@
+//# we should use do while loop only when we need to run the loop minimum one time.
+//! while loop
+let i = 4;
+while(i<100){
+    console.log(i)
+    i++;
+}
+
+
+//# do while will run minimum one time .
+//! do while loop  
+let i2 = 4;
+do{
+    console.log(i2)
+    i2++;
+
+}while(i2>100)
+
+    
