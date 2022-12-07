5ad823b9550967e37947617f682f8c728b49ca85
darkrabel <darkrabelstudy@gmail.com>
2022-12-07 13:55:39 +0600

    chapter 3 finished

---
+++ chapter3/function.js
@@ -0,0 +1,12 @@
+//! function 
+function sum(a,b){
+    return(a+b); //# this a , b will be local variable ;
+}
+console.log(sum(5,6))
+
+
+//! arow funtion 
+const sum2 = (a,b)=>{
+    return a+b;
+}
+console.log(sum2(2,3))
---
+++ chapter3/practiseset3.js
@@ -0,0 +1,28 @@
+//! questions  
+//? 1.    Write a program to print the marks of a student in an object using for loop 
+//?        obj = {harry: 98, rohan: 70, aakash: 7}
+//? 2.    Write a program in Q-1 using for in loop.
+//? 3.    Write a program to print “try again” until the user enters the correct number.
+//? 4.    Write a function to find mean of 5 numbers.
+//! ans of 1 and 2 
+obj = {harry: 98, rohan: 70, aakash: 7}
+
+for(a in obj){
+    console.log(obj[a])
+}
+
+
+
+
+let cn =43;
+let i ;
+while(cn!=i){
+    i=43;//#this is user given number 
+
+}
+console.log("you enterd a correct number ")
+const mean =(l,k,m,n)=>{
+    return(l+k+m+n)/4
+
+}
+console.log(mean(4,45,45,45))
